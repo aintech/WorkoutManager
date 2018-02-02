438fd7d7162f890b4e5a3b26da8be46fcf654946
unknown <yaremchuken@2013092001x.rca.ru>
2018-02-02 16:56:35 +0300

    Little more initial sql

--- src/main/resources/initial_schema.sql
+++ src/main/resources/initial_schema.sql
@@ -1,3 +1,17 @@
+create table _user (
+	_id int primary key not null,
+	_username varchar(255),
+	_password varchar(255)
+);
+create table _schedule (
+	_id int primary key not null,
+	_user_id int not null,
+	_name varchar(255)
+);
+create table _workout (
+	_id int primary key not null,
+	_name varchar(255)
+);
 create table _exercise (
 	_id int primary key not null,
 	_group varchar(25),
@@ -6,27 +20,13 @@ create table _exercise (
 	_weight int,
 	_external_link varchar(255)
 );
-create table _workout (
-	_id int primary key not null,
-	_name varchar(255)
-);
-create table _workout_exercise_binding (
-	_id int primary key not null,
-	_workout_id int not null,
-	_exercise_id int not null
-);
-create table _schedule (
-	_id int primary key not null,
-	_user_id int not null,
-	_name varchar(255)
-);
 create table _schedule_workout_binding (
 	_id int primary key not null,
 	_schedule_id int not null,
 	_workout_id int not null
 );
-create table _user (
+create table _workout_exercise_binding (
 	_id int primary key not null,
-	_username varchar(255),
-	_password varchar(255)
+	_workout_id int not null,
+	_exercise_id int not null
 );
